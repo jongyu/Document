5f2e61b195e294a0d6981c1d789b3c3d9cf21a24
Yu <zhongyu.vip@gmail.com>
2020-02-25 17:27:03 +0800

    Create ReflectUtils.java

---
+++ java/ReflectUtils.java
@@ -0,0 +1,29 @@
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author 钟宇
+ */
+public class ReflectUtils {
+
+    /**
+     * 通过springframework.core 包中相关的类获取指定类中指定方法名的所有参数名
+     *
+     * @param clazz      要获取参数的类名
+     * @param methodName 要获取参数的方法
+     * @return 获取到的参数
+     */
+    public static String[] getArgsNamesBySpring(Class<?> clazz, String methodName) {
+        ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                return parameterNameDiscoverer.getParameterNames(method);
+            }
+        }
+        return null;
+    }
+
+}
